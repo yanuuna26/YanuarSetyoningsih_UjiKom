5b6219693b28b11a7e554a354c2a98565e4d5eb8
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-20 13:32:13 +0700

    Main menu dan bgm

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource bgm_main_menu;
     void Start()
     {
-        
+        bgm_main_menu.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickStart()
     {
-        
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void onClickCredit()
+    {
+        SceneManager.LoadScene("Credits");
     }
 }
