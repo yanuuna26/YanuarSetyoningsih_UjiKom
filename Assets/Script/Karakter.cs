712480026aff833797429876752267f9110b69ac
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-20 10:53:02 +0700

    peluru

--- Assets/Script/Karakter.cs
+++ Assets/Script/Karakter.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Karakter : MonoBehaviour
 {
-    //public Transform posisiKarakter;
+    public GameObject posisiKarakter;
     public Vector2 posisiMouse;
+    public GameObject peluru;
+    public float forceMultiplier = 10.0f;
+    public float jumpForce = 5f;
+    public Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,13 +17,27 @@ public class Karakter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        geserKarakter();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            float setengahTinggi = Camera.main.orthographicSize;
+            float setengahLebar = setengahTinggi * Camera.main.aspect;
+            Vector2 posisiMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 posisiKananKiri = new Vector2(posisiMouse.x, transform.position.y); //drag kanan kiri saja
+            posisiKananKiri.x = Mathf.Clamp(posisiKananKiri.x, -setengahLebar, setengahLebar);
+            //Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.90f, 10f));
+            Instantiate(peluru, posisiKananKiri, Quaternion.identity);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    public void geserKarakter()
+    {
         float setengahTinggi = Camera.main.orthographicSize;
         float setengahLebar = setengahTinggi * Camera.main.aspect;
         Vector2 posisiMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 posisiKananKiri = new Vector2(posisiMouse.x, transform.position.y); //drag kanan kiri saja
         posisiKananKiri.x = Mathf.Clamp(posisiKananKiri.x, -setengahLebar, setengahLebar); //drag kanan kiri sesuai ukuran scene
-        // posisiMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // transform.position = Vector2.MoveTowards(transform.position, posisiMouse, 0.05f);
         transform.position = posisiKananKiri;
     }
 }
